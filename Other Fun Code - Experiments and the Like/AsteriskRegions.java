ac5aa2635045cd82c3ff7f699b74052e63db838e
Jaewon Lee <joyjwlee@gmail.com>
2020-01-11 21:13:39 -0800

    Program to find number of asterisk regions in a 2D character array - creds to Ivan H.

---
+++ Other Fun Code - Experiments and the Like/AsteriskRegions.java
@@ -0,0 +1,55 @@
+//Objective: Find number of asterisk regions
+
+import java.util.*;
+public class AsteriskRegions {
+    static int N, M;
+    //static final int MAX = 1001; //1000 is upper bound of N and M
+
+    public static void main (String args[]) {
+        for(int b = 1; b <= 6; b++) {//Read in N and M
+            System.out.println("\n\n===Test " + b+ "===");
+            Scanner sc = new Scanner(System.in);
+            N = sc.nextInt();
+            M = sc.nextInt();
+
+            //Now read in character array
+            char[][] arr = new char[N][M];
+            for (int i = 0; i < N; i++) {
+                String temp;
+                temp = sc.next();
+                for (int j = 0; j < M; j++) {
+                    arr[i][j] = temp.charAt(j);
+                }
+            }
+
+            System.out.println("Print character array: ");
+            for (int i = 0; i < N; i++) { 
+                for (int j = 0; j < M; j++) { 
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            int counter = 0;
+            for (int i = 0; i < N; i++) { 
+                boolean cInc = false;
+                for (int j = 0; j < M; j++) {
+                    if ( cInc && arr[i][j] == '*' && (j != 0)  && (i != 0 && arr[i-1][j] == '*')) {
+                        counter--;
+                        System.out.println("Counter decreased\tCoordinates: (" + i + ", " + j + ")");
+                    }
+
+                    if (arr[i][j] == '*' && (j == 0 || arr[i][j - 1] == '.') && (i == 0 || arr[i-1][j] == '.')) {
+                        counter++;
+                        cInc = true;
+                        System.out.println("Counter increased\tCoordinates: (" + i + ", " + j + ")");
+                    } else if(arr[i][j] == '.')
+                        cInc = false;
+                }
+            }
+
+            //Output answer
+            System.out.println("==> " + counter);
+        }
+    }
+}
