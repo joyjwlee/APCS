ce4302fa05fb14eb23278b73580d4863be9ca559
Jaewon Lee <joyjwlee@gmail.com>
2020-05-08 14:08:34 -0700

    practice for ap exam

--- Other Fun Code - Experiments and the Like/practice.java
+++ Other Fun Code - Experiments and the Like/practice.java
@@ -1,3 +1,4 @@
+// May 5
 public int findLowestScore() {
     int lowest = players[0].getScore();
     for (int i = 0; i < players.length; i++) {
@@ -56,3 +57,58 @@ public String playGame() {
     return players[0].getName();
 }
 // enhancement: arraylist for players, not array
+
+/*************************************************************************************/
+
+// May 8
+private void cleanData() {
+    for (int i = 0; i < pings.size(); i++) {
+        if (pings.get(i).atHome() || pings.get(i).getDuration() < limit) {
+            pings.remove(i);
+            i--;
+        }
+    }
+}
+
+public boolean unsafeHour(int hour, int min, int threshold) {
+    cleanData();
+    int num = 0;
+    int seconds = 0;
+    for (int i = 0; i < pings.size(); i++) {
+        if (pings.get(i).getHour() == hour) {
+            seconds += pings.get(i).getDuration();
+            num++;
+        }
+    }
+    if (num > min || seconds > threshold * num) {
+        return true;
+    }
+    return false;
+}
+
+public int nonCompliantUsers() {
+    int ans = 0;
+    for (int i = 0; i < users.length; i++) {
+        int hours = 0;
+        for (int j = 0; j < 24; j++) {
+            if (users[i].unsafeHour(j, allowed, avergeLimit)) {
+                hours++;
+            }
+        }
+        if (hours > 3) {
+            ans++;
+        }
+    }
+    return ans;
+}
+
+//Goes through each user and identifies if they are egregious or not
+//Returns an arraylist of strings that has names of the egregious users
+public ArrayList<String> egregiousUsers() {
+    /*
+        Modifications:
+        - None in ping
+        - DailyUserData, public string for name and getName() method; also modify constructor
+        - Analyzer, have a public arraylist of string and a method that updates list of egregious users  
+    */
+}
