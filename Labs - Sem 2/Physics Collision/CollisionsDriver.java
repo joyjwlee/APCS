51c48e95a896b72038899d4567535ea6b0ed4754
Jaewon Lee <joyjwlee@gmail.com>
2020-05-28 09:03:28 -0700

    more edits coming soon

--- Labs - Sem 2/Physics Collision/CollisionsDriver.java
+++ Labs - Sem 2/Physics Collision/CollisionsDriver.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CollisionsDriver {
+    // JFrame object
     static JFrame frame;
 
     public static void main(String[] args) {
-        // create a JFrame (window) that will be visible on screen
-        frame = new JFrame("Elastic Collision Simulator");
+        frame = new JFrame("Elastic Collision Simulator"); // create Jframe and give title
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // make the red close button work
         frame.setLocation(50, 50); // place the frame in the upper left corner
 
@@ -14,10 +14,10 @@ public class CollisionsDriver {
 
         frame.setPreferredSize(new Dimension(1920, 1080)); // set frame 1920x1080
         frame.setResizable(false); // cannot resize
-        frame.add(engine); // add game to the frame so it will be on the screen
-        frame.pack();
-        frame.setVisible(true);
-        engine.animate();// call the playGame() method to intitiate the game
+        frame.add(engine); // add engine to the frame so it will be on the screen
+        frame.pack(); // pack frame
+        frame.setVisible(true); // make it able to be seen
+        engine.animate(); // call the animate() method to intitiate the game
     }
 
     public static JFrame getFrame() {
